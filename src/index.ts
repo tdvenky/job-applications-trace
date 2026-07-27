ec6ab69ea13b3d9b87dad5be2771350e7677a13f
Venkatesh Thimma Dhinakaran <venky.td@gmail.com>
2026-07-26 20:52:28 -0400

    Bump CLI version string to 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const program = new Command();
 program
   .name('job-applications-trace')
   .description('AI agent that reconstructs your job search from Gmail and Calendar')
-  .version('0.1.0');
+  .version('0.2.0');
 
 program
   .command('auth')
